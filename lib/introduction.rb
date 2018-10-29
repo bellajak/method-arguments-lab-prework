3ca35a21857c93d805b94a9cd2f20d11f458a8fd
Jakob Bellamy <jakbellamy@gmail.com>
2018-10-29 20:15:38 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 def introduction(name, programming_language)
-  puts "Hi, my name is" name " and I am learning to program in" programming_language "."
+  puts "Hi, my name is #{name} and I am learning to program in #{programming_language}."
 end
 
 puts introduction("dan", "Ember.js")
